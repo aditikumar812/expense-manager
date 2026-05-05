5b4309e8b31ab6897d245473583da5d092d87279
Aditi <aditi.kumar812@gmail.com>
2026-05-05 11:12:26 +0300

    Added delete expense feature w confirmation

--- app.py
+++ app.py
@@ -31,15 +31,28 @@ def addexpense():
         return render_template('add.html')
     
 
-@app.route('/viewexpense')# why didnt we write methods? cuz we need only get to view and get is by default
+@app.route('/viewexpense')# why didnt we write methods? cuz we need only 'get' to view and 'get' is by default
 def viewexpense():
     mycursor=con.cursor()
     mycursor.execute('Select * from expenses;')
     expenses=mycursor.fetchall()
     return render_template ('view.html',expenses=expenses)
+
+
+@app.route('/deleteexpense', methods=['GET', 'POST'])
+def deleteexpense():
+    mycursor=con.cursor() # no need to write if req method=post cuz alwas post hi hoga kya get?
+    id=request.form['id']
+    mycursor.execute('Delete from expenses where id=%s',(id,))
+    con.commit()
+    return redirect(url_for('viewexpense'))
+    
+
+
         
         
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
